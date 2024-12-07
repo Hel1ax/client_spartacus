cbc8ee9f9bde9087a3d5654005e26a33bba8ae3c
lanre <arsenmahtaev@gmail.com>
2024-12-07 17:20:08 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: ['file-loader'],
+            },
+            { 
+                test: /\.css$/, 
+                use: [ 'style-loader', 'css-loader', 'postcss-loader' ] 
+            },
+            { 
+                test: /\.(ts|tsx|js)$/,
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'ts-loader', 
+                    options: {
+                        compilerOptions: {
+                            noEmit: false, 
+                        },
+                    } 
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html'),
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/images', to: 'images' }
+            ]
+        })
+    ],
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+
+    devServer: {
+        hot: true, 
+        port: 8080,
+        historyApiFallback: true,
+        proxy: [{
+            context: ['/api'],
+            target: 'https://server-spartacus.onrender.com/',
+        }],
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+}
