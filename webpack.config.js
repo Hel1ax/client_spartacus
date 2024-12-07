ef9d94da89e1721a792274ba8d1c94f02595b77f
lanre <arsenmahtaev@gmail.com>
2024-12-07 17:26:43 +0300

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -56,12 +56,16 @@ module.exports = {
 
     devServer: {
         hot: true, 
-        port: 8080,
+        port: 3000,
+        disableHostCheck: true,
         historyApiFallback: true,
+        public: 'https://server-spartacus.onrender.com',
         proxy: [{
             context: ['/api'],
-            target: 'https://server-spartacus.onrender.com/',
+            target: 'https://server-spartacus.onrender.com',
         }],
+
+        allowedHosts: 'all'
     },
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 }
